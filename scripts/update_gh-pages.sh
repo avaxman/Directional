87cd4ddac501b4d73664148fb5f9c683792ac7ee
Amir Vaxman <a.vaxman@uu.nl>
2017-11-07 21:53:16 +0100

    no message

--- scripts/update_gh-pages.sh
+++ scripts/update_gh-pages.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Usage: cd $LIBIGL; scripts/update_gh-pages.sh
+# Usage: cd $LIBDIRECTIONAL; scripts/update_gh-pages.sh
 set -o xtrace
 
-HEADER="title: libigl
-author: Alec Jacobson and Daniele Panozzo and others
+HEADER="title: libdirectional
+author: Amir Vaxman and others
 css: tutorial/style.css
 html header:   <script type='text/javascript' src='http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'></script>
 <link rel='stylesheet' href='http://yandex.st/highlightjs/7.3/styles/default.min.css'>
@@ -15,11 +15,9 @@ html header:   <script type='text/javascript' src='http://cdn.mathjax.org/mathja
 echo "$HEADER" \
   | cat - README.md | multimarkdown -o index.html
 
-echo "$HEADER" \
-  | cat - style-guidelines.md | multimarkdown -o style-guidelines.html
 
-HEADER="title: libigl
-author: Alec Jacobson and Daniele Panozzo and others
+HEADER="title: libdirectional
+author: Amir Vaxman and others
 css: ../tutorial/style.css
 html header:   <script type='text/javascript' src='http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'></script>
 <link rel='stylesheet' href='http://yandex.st/highlightjs/7.3/styles/default.min.css'>
@@ -28,10 +26,5 @@ html header:   <script type='text/javascript' src='http://cdn.mathjax.org/mathja
 
 "
 
-echo "$HEADER" \
-  | cat - optional/README.md | multimarkdown -o optional/index.html
-
 multimarkdown tutorial/tutorial.md -o tutorial/tutorial.html
 
-echo "$HEADER" \
-  | cat - coding-guidelines.md | multimarkdown -o coding-guidelines.html
