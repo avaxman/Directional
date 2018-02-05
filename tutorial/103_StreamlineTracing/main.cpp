ae47eaf3cc87d64a193e67679d807b78653b8e0b
Amir Vaxman <amirvaxman@amirs-imac.cs.uu.nl>
2018-02-05 17:57:13 +0100

    103 works.

--- tutorial/103_StreamlineTracing/main.cpp
+++ tutorial/103_StreamlineTracing/main.cpp
@@ -14,6 +14,7 @@
 #include <igl/viewer/Viewer.h>
 #include <directional/complex_field.h>
 #include <directional/complex_to_raw.h>
+#include <directional/line_cylinders.h>
 
 #include <cstdlib>
 #include <iostream>
@@ -24,6 +25,10 @@
 // Mesh
 Eigen::MatrixXd V;
 Eigen::MatrixXi F;
+
+Eigen::MatrixXd fullV;
+Eigen::MatrixXi fullF;
+Eigen::MatrixXd fullC;
 
 Eigen::VectorXi cIDs;
 Eigen::MatrixXd cValues;
@@ -43,98 +48,102 @@ int anim_t_dir = 1;
 
 bool pre_draw(igl::viewer::Viewer &viewer)
 {
-    using namespace Eigen;
-    using namespace std;
-
-    if (!viewer.core.is_animating)
-        return false;
-
-    igl::streamlines_next(V, F, sl_data, sl_state);
-    Eigen::RowVector3d color = Eigen::RowVector3d::Zero();
-    double value = ((anim_t) % 100) / 100.;
-
-    if (value > 0.5)
-        value = 1 - value;
-    value = value / 0.5;
-    igl::parula(value, color[0], color[1], color[2]);
-
-    viewer.data.add_edges(sl_state.start_point, sl_state.end_point, color);
-
-    anim_t += anim_t_dir;
-
+  using namespace Eigen;
+  using namespace std;
+  
+  if (!viewer.core.is_animating)
     return false;
+  
+  
+  igl::streamlines_next(V, F, sl_data, sl_state);
+  Eigen::RowVector3d color = Eigen::RowVector3d::Zero();
+  double value = ((anim_t) % 100) / 100.;
+  
+  if (value > 0.5)
+    value = 1 - value;
+  value = value / 0.5;
+  igl::parula(value, color[0], color[1], color[2]);
+  
+  directional::line_cylinders(sl_state.start_point, sl_state.end_point, 0.0005, Eigen::MatrixXd::Constant(sl_state.start_point.rows(),3,1.0), 4, true, true, fullV, fullF, fullC);
+  //viewer.data.add_edges(sl_state.start_point, sl_state.end_point, color);
+  
+  viewer.data.clear();
+  viewer.data.set_mesh(fullV, fullF);
+  viewer.data.set_colors(fullC);
+  anim_t += anim_t_dir;
+  
+  return false;
 }
 
 bool key_down(igl::viewer::Viewer &viewer, unsigned char key, int modifier)
 {
-    if (key == ' ')
-    {
-        viewer.core.is_animating = !viewer.core.is_animating;
-        return true;
-    }
-    return false;
+  if (key == ' ')
+  {
+    viewer.core.is_animating = !viewer.core.is_animating;
+    return true;
+  }
+  return false;
 }
 
 int main(int argc, char *argv[])
 {
-    using namespace Eigen;
-    using namespace std;
-
-
-    // Load a mesh in OFF format
-    igl::readOFF(TUTORIAL_SHARED_PATH "/bumpy.off", V, F);
-    // Create a Vector Field
-    Eigen::VectorXi b;
-    Eigen::MatrixXd bc;
-    Eigen::VectorXd S; // unused
-
-    b.resize(1);
-    b << 0;
-    bc.resize(1, 3);
-    bc << 1, 1, 1;
-
-    half_degree = 3;
-    treat_as_symmetric = true;
-
-    Eigen::MatrixXd temp_field, temp_field2;
-    //igl::copyleft::comiso::nrosy(V, F, b, bc, VectorXi(), VectorXd(), MatrixXd(), 1, 0.5, temp_field, S);
+  using namespace Eigen;
+  using namespace std;
   
-    directional::complex_field(V, F, b,  bc , 4, complexField);
   
-    // Convert it to raw field
-    directional::complex_to_raw(V,F,complexField,4,raw, true);
+  // Load a mesh in OFF format
+  igl::readOFF(TUTORIAL_SHARED_PATH "/lion.off", V, F);
+  // Create a Vector Field
+  Eigen::VectorXi b;
+  Eigen::MatrixXd bc;
+  Eigen::VectorXd S; // unused
+  
+  b.resize(1);
+  b << 0;
+  bc.resize(1, 3);
+  bc << 1, 1, 1;
+  
+  fullV=V;
+  fullF=F;
 
-    igl::streamlines_init(V, F, raw, false, sl_data, sl_state);
+  half_degree = 3;
+  treat_as_symmetric = true;
+  
+  Eigen::MatrixXd temp_field, temp_field2;
+  //igl::copyleft::comiso::nrosy(V, F, b, bc, VectorXi(), VectorXd(), MatrixXd(), 1, 0.5, temp_field, S);
+  
+  directional::complex_field(V, F, b,  bc , 4, complexField);
+  
+  // Convert it to raw field
+  directional::complex_to_raw(V,F,complexField,4,raw, true);
+  
+  igl::streamlines_init(V, F, raw, false, sl_data, sl_state);
+  
+  // Viewer Settings
+  igl::viewer::Viewer viewer;
+  viewer.callback_pre_draw = &pre_draw;
+  viewer.callback_key_down = &key_down;
+  viewer.core.show_lines = false;
+  viewer.core.is_animating = false;
+  viewer.core.animation_max_fps = 30.;
+  
+  // Paint mesh grayish
+  fullC.setConstant(V.rows(), 3, 0.1);
 
-    // Viewer Settings
-    igl::viewer::Viewer viewer;
-    viewer.data.set_mesh(V, F);
-    viewer.callback_pre_draw = &pre_draw;
-    viewer.callback_key_down = &key_down;
-
-    viewer.core.show_lines = false;
-
-    viewer.core.is_animating = false;
-    viewer.core.animation_max_fps = 30.;
-
-    // Paint mesh grayish
-    Eigen::MatrixXd C;
-    C.setConstant(viewer.data.V.rows(), 3, 0.1);
-    viewer.data.set_colors(C);
-
-
-    // Draw vector field on sample points
-    igl::StreamlineState sl_state0;
-    sl_state0 = sl_state;
-    igl::streamlines_next(V, F, sl_data, sl_state0);
-    Eigen::MatrixXd v = sl_state0.end_point - sl_state0.start_point;
-    v.rowwise().normalize();
-
-    viewer.data.add_edges(sl_state0.start_point,
-                          sl_state0.start_point + 0.059 * v,
-                          Eigen::RowVector3d::Constant(1.0f));
-
-    cout <<
-    "Press [space] to toggle animation" << endl;
-    viewer.launch();
+  // Draw vector field on sample points
+  igl::StreamlineState sl_state0;
+  sl_state0 = sl_state;
+  igl::streamlines_next(V, F, sl_data, sl_state0);
+  Eigen::MatrixXd v = sl_state0.end_point - sl_state0.start_point;
+  v.rowwise().normalize();
+  
+  directional::line_cylinders(sl_state0.start_point, sl_state0.start_point + 0.0005 * v, 0.0005, Eigen::MatrixXd::Constant(sl_state0.start_point.rows(),3,1.0), 4, true, true, fullV, fullF, fullC);
+  
+  viewer.data.clear();
+  viewer.data.set_mesh(fullV, fullF);
+  viewer.data.set_colors(fullC);
+  
+  cout <<
+  "Press [space] to toggle animation" << endl;
+  viewer.launch();
 }
