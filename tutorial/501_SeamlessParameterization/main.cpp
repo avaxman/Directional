42f9d2fbcfbc43f5426b7c6ef8439f3f85f15b2f
Amir Vaxman <a.vaxman@uu.nl>
2018-06-01 04:13:30 +0200

    Changed mesh in 501.

--- tutorial/501_SeamlessParameterization/main.cpp
+++ tutorial/501_SeamlessParameterization/main.cpp
@@ -117,8 +117,8 @@ int main()
   "  0  Toggle raw field/Combed field" << std::endl <<
   "  1  Show/hide singularities" << std::endl <<
   "  2  Show textured mesh/original mesh" << std::endl <<
-  igl::readOBJ(TUTORIAL_SHARED_PATH "/lilium.obj", wholeV, wholeF);
-  directional::read_raw_field(TUTORIAL_SHARED_PATH "/lilium.rawfield", N, rawField);
+  igl::readOBJ(TUTORIAL_SHARED_PATH "/sphere.obj", wholeV, wholeF);
+  directional::read_raw_field(TUTORIAL_SHARED_PATH "/sphere_param.rawfield", N, rawField);
   igl::edge_topology(wholeV, wholeF, EV, FE, EF);
   
   //computing
