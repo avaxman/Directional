36dd5e075f299c12fb634ff33c55736ace736307
Amir Vaxman <>
2018-09-28 18:14:06 +0200

    Still struggling with hexes a bit.

--- include/directional/parameterize.h
+++ include/directional/parameterize.h
@@ -273,7 +273,7 @@ namespace directional
     }
     
     //the results are packets of N functions for each vertex, and need to be allocated for corners
-    //cout<<"fullx: "<<fullx<<endl;
+    cout<<"fullx: "<<fullx<<endl;
     VectorXd cutUVWVec=vt2cMat*symmMat*fullx;
     cutUVW.conservativeResize(cutV.rows(),N/2);
     cutUV.conservativeResize(cutV.rows(),2);
@@ -286,11 +286,14 @@ namespace directional
     if (N==4){
       cutUV = cutUVW.block(0,0,cutUVW.rows(),2);
     }else {
+      double yratio = 2.0/sqrt(3.0);
       RowVector3d UAxis; UAxis<<1, 1,0; UAxis.normalize();
       RowVector3d VAxis; VAxis<<-1, 1, 2; VAxis.normalize();
-      for (int i=0;i<cutV.rows();i++)
-        cutUV.row(i)<<cutUVWVec.segment(N*i,N/2).transpose().dot(UAxis)/3.0,cutUVWVec.segment(N*i,N/2).transpose().dot(VAxis)/sqrt(3.0);
-      
+      VectorXd a = cutUVW.col(0)+cutUVW.col(1);
+      VectorXd b = -cutUVW.col(0)+cutUVW.col(2);
+      cutUV.col(0) = a-b/2.0;
+      cutUV.col(1) = b*sqrt(3.0)/2.0*yratio;
+      //cutUV = cutUVW.block(0,0,cutUVW.rows(),2);
     }
   }
 }
