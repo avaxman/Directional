820de399ee8430ce838c29ce16c8fa047e9f380e
Amir Vaxman <>
2018-11-12 18:05:45 +0100

    Started Hodge Decomposition.

--- tutorial/602_HodgeDecomposition/main.cpp
+++ tutorial/602_HodgeDecomposition/main.cpp
@@ -65,14 +65,78 @@ bool key_down(igl::opengl::glfw::Viewer& viewer, int key, int modifiers)
 
 int main()
 {
+  using namespace Eigen;
   std::cout <<"<space bar>  Show/hide Singularities" << std::endl;
   
-  igl::readOFF(TUTORIAL_SHARED_PATH "/bumpy.off", VMesh, FMesh);
-  directional::read_raw_field(TUTORIAL_SHARED_PATH "/bumpy.rawfield", N, rawField);
-  directional::read_singularities(TUTORIAL_SHARED_PATH "/bumpy.sings", N, singVertices, singIndices);
+  igl::readOFF(TUTORIAL_SHARED_PATH "/rocker-arm2500.obj", VMesh, FMesh);
+  directional::read_raw_field(TUTORIAL_SHARED_PATH "/rocker-arm2500.rawfield", N, rawField);
   
-  // Set colors for Singularities
+ 
+  directional::FEM_suite(VMesh, FMesh, EV, FE, EF, Gv, Ge, J, C, D);
+  directional::FEM_masses(VMesh, FMesh, EV, FE, EF, MvVec, MeVec, MfVec, MchiVec);
   
+  igl::diag(MvVec,Mv);
+  igl::diag(MeVec,Me);
+  igl::diag(MfVec,Mf);
+  igl::diag(MchiVec,Mchi);
+  
+  Lv = D*Gv;   //Gv^T * Mchi * Gv
+  Le = C*J*Ge; //(JGe)^T * Mchi * JGe
+  
+  //solving for exact part
+   VectorXd scalarFunc;
+  igl::min_quad_with_fixed_data<double> mqwfExact;
+  // Linear term is 0
+  VectorXd B = VectorXd::Zero(V.rows(),1);
+  VectorXd Beq;
+  SparseMatrix<double> Aeq;
+  Eigen::VectorXi b(1); b(0)=0;
+  Eigen::VectorXd bc(1); bc(0)=0;
+  
+  igl::min_quad_with_fixed_precompute(Lv,b,Aeq,true,mqwfExact);
+  igl::min_quad_with_fixed_solve(mqwfExact,B,bc,Beq,scalarFunc);
+  
+  VectorXd exactFunc(V.rows());
+  exactFunc<<0.0,scalarFunc;
+  
+  //solving for coexact part
+  igl::min_quad_with_fixed_data<double> mqwfCoexact;
+  // Linear term is 0
+  VectorXd B = VectorXd::Zero(V.rows(),1);
+  VectorXd Beq;
+  SparseMatrix<double> Aeq;
+  Eigen::VectorXi b(1); b(0)=0;
+  Eigen::VectorXd bc(1); bc(0)=0;
+  
+  igl::min_quad_with_fixed_precompute(Le,b,Aeq,true,mqwfCoexact);
+  igl::min_quad_with_fixed_solve(mqwfCoexact,B,bc,Beq,scalarFunc);
+  
+  VectorXd exactFunc(V.rows());
+  exactFunc<<0.0,scalarFunc;
+
+  
+  Eigen::VectorXd gradFieldVec = Gv*vScalar;
+  Eigen::VectorXd rotCogradFieldVec = J*Ge*eScalar;
+  
+  gradField.resize(FMesh.rows(),3);
+  rotCogradField.resize(FMesh.rows(),3);
+  for (int i=0;i<FMesh.rows();i++)
+    for (int j=0;j<3;j++){
+      gradField(i,j)=gradFieldVec(3*i+j);
+      rotCogradField(i,j)=rotCogradFieldVec(3*i+j);
+    }
+  
+  fieldDiv = D*gradFieldVec;
+  fieldCurl = C*rotCogradFieldVec;
+  
+  //visualization meshes
+  directional::non_conforming_mesh(VMesh, FMesh, EV, FE, EF, eScalar, VNCMesh, FNCMesh, CNCMesh);
+  directional::edge_diamond_mesh(VMesh, FMesh, EV, FE, EF, fieldCurl.cwiseAbs(), VEDMesh, FEDMesh, CEDMesh);
+  directional::vertex_area_mesh(VMesh, FMesh, EV, FE, EF, fieldDiv.cwiseAbs(), VVAMesh, FVAMesh, CVAMesh);
+  
+  std::cout<<"Structure-preserving sanity check: "<<std::endl;
+  std::cout<<"(C*gradField).lpNorm<Infinity>(): "<<(C*gradFieldVec).lpNorm<Eigen::Infinity>()<<std::endl;
+  std::cout<<"(D*rotCogradField).lpNorm<Infinity>(): "<<(D*rotCogradFieldVec).lpNorm<Eigen::Infinity>()<<std::endl;
   
   create_meshes();
   viewer.callback_key_down = &key_down;
