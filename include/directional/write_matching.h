265aca47156c7ee95756c371a6356b7f2067a984
Kacper Pluta <kacper.pluta@bsdlabs.com.br>
2020-08-04 01:51:10 +0300

    add missing support for the matching file format

--- include/directional/write_matching.h
+++ include/directional/write_matching.h
@@ -4,8 +4,8 @@
 // This Source Code Form is subject to the terms of the Mozilla Public License
 // v. 2.0. If a copy of the MPL was not distributed with this file, You can
 // obtain one at http://mozilla.org/MPL/2.0/.
-#ifndef DIRECTIONAL_READ_MATCHING_H
-#define DIRECTIONAL_READ_MATCHING_H
+#ifndef DIRECTIONAL_WRITE_MATCHING_H
+#define DIRECTIONAL_WRITE_MATCHING_H
 #include <cmath>
 #include <Eigen/Core>
 #include <string>
@@ -17,30 +17,31 @@
 namespace directional
 {
 
-	// Reads the vector field matching into a file. For the file format specification see: https://avaxman.github.io/Directional/file_formats/
+	// Writes the vector field matching into a file. For the file format specification see: https://avaxman.github.io/Directional/file_formats/
 	// Inputs:
 	//   fileName:  The to be loaded file.
-    // Outputs:
     //   matching:  The matching per edge 
     //   EF:        The edge to face matching
 	//   N:         The degree of the field
 	// Return:
 	//   Whether or not the file was written successfully
-  bool IGL_INLINE read_matching(const std::string &fileName,
-                                Eigen::VectorXi& matching,
-                                Eigen::MatrixXi& EF,
-                                int & N)
+  bool IGL_INLINE write_matching(const std::string &fileName,
+                                const Eigen::VectorXi& matching,
+                                const Eigen::MatrixXi& EF,
+                                int N)
 	{
 		try
 		{
-            std::ifstream f(fileName);
-            int numEdges = 0;
-			f >> N >> numEdges;
-            matching.conservativeResize(numEdges);
-            EF.conservativeResize(numEdges,2);
+            std::ofstream f(fileName);
             
-            for (int i=0;i<numEdges;i++)
-                f >> EF(i,0)>> EF(i,1) >> matching(i);
+            if (! f.is_open())
+                return false;
+            
+			int numEdges = EF.rows();
+			
+            f << N << " " << numEdges << std::endl;
+            for (int i = 0; i < numEdges; i++)
+                f << EF(i,0) << " " << EF(i,1) << " " << matching(i) << std::endl;
       
 			f.close();
 			return f.fail();
@@ -49,6 +50,7 @@ namespace directional
 		{
 			return false;
 		}
+		return true;
 	}
 }
 
