64affb67881f5762540e5897c7a4c58a85ee6bc6
Amir Vaxman <a.vaxman@uu.nl>
2021-11-28 16:10:11 +0100

    Tutorial UI is done.

--- tutorial/303_FlexibleConstraints/main.cpp
+++ tutorial/303_FlexibleConstraints/main.cpp
@@ -30,7 +30,7 @@ int N = 4;
 typedef enum {CONSTRAINTS, HARD_PRESCRIPTION, SOFT_PRESCRIPTION} ViewingModes;
 ViewingModes viewingMode=CONSTRAINTS;
 
-bool normalized=false;
+bool alterRoSyWeight=false;
 
 void recompute_field()
 {
@@ -49,10 +49,7 @@ void update_visualization()
   if (viewingMode==HARD_PRESCRIPTION){
     viewer.set_selected_faces(Eigen::VectorXi());
     directional::polyvector_to_raw(B1,B2, pvFieldHard, N, rawFieldHard, N%2==0);
-    if (normalized)
-      for(int n = 0; n < N; n++)
-        rawFieldHard.middleCols(n*3, 3).rowwise().normalize();
-    
+   
     directional::principal_matching(V, F, EV, EF, FE, rawFieldHard, matching, effort, singVertices, singIndices);
     viewer.set_field(rawFieldHard);
     viewer.set_singularities(singVertices, singIndices);
@@ -61,9 +58,6 @@ void update_visualization()
   if (viewingMode==SOFT_PRESCRIPTION){
     viewer.set_selected_faces(Eigen::VectorXi());
     directional::polyvector_to_raw(B1, B2, pvFieldSoft, N, rawFieldSoft);
-    if (normalized)
-      for(int n = 0; n < N; n++)
-        rawFieldSoft.middleCols(n*3, 3).rowwise().normalize();
     
     directional::principal_matching(V, F, EV, EF, FE, rawFieldSoft, matching, effort, singVertices, singIndices);
     viewer.set_field(rawFieldSoft);
@@ -80,52 +74,53 @@ bool key_down(igl::opengl::glfw::Viewer& viewer, int key, int modifiers)
   {
       // Select vector
     case '1':
-      roSyWeight*=2.0;
-      std::cout<<"roSyWeight: "<<roSyWeight<<std::endl;
+      alterRoSyWeight = !alterRoSyWeight;
+      if (alterRoSyWeight)
+        std::cout<<"Altering RoSy weight"<<std::endl;
+      else
+        std::cout<<"Altering alignment weight"<<std::endl;
+      break;
+      
+    case 'P':
+      if (alterRoSyWeight){
+        roSyWeight*=2.0;
+        std::cout<<"roSyWeight: "<<roSyWeight<<std::endl;
+      }else{
+        alignWeights.array()*=2.0;
+        std::cout<<"alignWeights: "<<alignWeights(1)<<std::endl;
+      }
       recompute_field();
       break;
+      
+    case 'M':
+      if (alterRoSyWeight){
+        roSyWeight/=2.0;
+        std::cout<<"roSyWeight: "<<roSyWeight<<std::endl;
+      }else{
+        alignWeights.array()/=2.0;
+        std::cout<<"alignWeights: "<<alignWeights(1)<<std::endl;
+      }
+      recompute_field();
+      break;
+      
     case '2':
-      roSyWeight/=2.0;
-      roSyWeight = (roSyWeight < 0.0 ? 0.0 : roSyWeight);
-      std::cout<<"roSyWeight: "<<roSyWeight<<std::endl;
-      recompute_field();
-      break;
-      
-    case '3':
-      alignWeights.array()*=2.0;
-      std::cout<<"alignWeights: "<<alignWeights(1)<<std::endl;
-      recompute_field();
-      break;
-      
-    case '4':
-      alignWeights.array()/=2.0;
-      alignWeights = (alignWeights(1) < 0.0 ? Eigen::VectorXd::Zero(alignWeights.size()) : alignWeights);
-      std::cout<<"alignWeights: "<<alignWeights(1)<<std::endl;
-      recompute_field();
-      break;
-      
-    case '5':
       viewingMode=CONSTRAINTS;
       break;
       
-    case '6':
-      viewingMode=SOFT_PRESCRIPTION;
-      break;
-      
-    case '7':
+    case '3':
       viewingMode=HARD_PRESCRIPTION;
       break;
-    
-    case 'N':
-      normalized = !normalized;
-      break;
       
-    /*case 'W':
-      if (directional::write_raw_field(TUTORIAL_SHARED_PATH "/fandisk.rawfield", rawField))
+    case '4':
+      viewingMode=SOFT_PRESCRIPTION;
+      break;
+    
+    case 'W':
+      if (directional::write_raw_field(TUTORIAL_SHARED_PATH "/fandisk.rawfield", (viewingMode==HARD_PRESCRIPTION ? rawFieldHard : rawFieldSoft)))
         std::cout << "Saved raw field" << std::endl;
       else
         std::cout << "Unable to save raw field. " << std::endl;
-      break;*/
+      break;
       
       
   }
@@ -139,11 +134,12 @@ int main()
 {
   
   std::cout <<
-  "  0+L-bttn   Choose face" << std::endl <<
-  "  0+R-bttn   Edit vector in current face" << std::endl<<
-  "  1          Choose vector in current face." << std::endl <<
-  "  R          Reset the constraints" << std::endl <<
-  "  N          Toggle field normalization" << std::endl;
+  "  1    Choose either RoSy or Alignment weight altering (default: RoSy)" << std::endl <<
+  "  +-   Alter (RoSy or Alignment) weight" << std::endl<<
+  "  2    Show constraints" << std::endl <<
+  "  3    Show fixed-alignment result" << std::endl <<
+  "  4    Shoe soft-alignment result" << std::endl <<
+  "  W    Save field"<< std::endl;
   
   // Load mesh
   igl::readOFF(TUTORIAL_SHARED_PATH "/fandisk.off", V, F);
