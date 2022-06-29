6f1cd73ca9afe8f4f5f8136aeb187a7e79b1d969
Amir Vaxman <>
2022-06-29 10:12:54 +0200

    505 works.

--- tutorial/505_Meshing/main.cpp
+++ tutorial/505_Meshing/main.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
 #include <Eigen/Core>
-#include <igl/opengl/glfw/Viewer.h>
-#include <igl/read_triangle_mesh.h>
-#include <igl/per_face_normals.h>
 #include <igl/unproject_onto_mesh.h>
-#include <igl/edge_topology.h>
-#include <igl/cut_mesh.h>
+#include <directional/TriMesh.h>
+#include <directional/FaceField.h>
+#include <directional/readOFF.h>
 #include <directional/read_raw_field.h>
 #include <directional/write_raw_field.h>
 #include <directional/curl_matching.h>
 #include <directional/combing.h>
 #include <directional/setup_integration.h>
 #include <directional/integrate.h>
-#include <directional/cut_mesh_with_singularities.h>
 #include <directional/branched_isolines.h>
 #include <directional/mesh_function_isolines.h>
 #include <directional/setup_mesh_function_isolines.h>
@@ -23,16 +20,11 @@
 
 int N[NUM_N];
 int currN = 0;
-Eigen::MatrixXi FMeshWhole, FMeshCut[NUM_N];
-Eigen::MatrixXd VMeshWhole, VMeshCut[NUM_N];
-Eigen::MatrixXd rawField[NUM_N], combedField[NUM_N];
-Eigen::VectorXd effort[NUM_N], combedEffort[NUM_N];
-Eigen::VectorXi matching[NUM_N], combedMatching[NUM_N];
-Eigen::MatrixXi EV, FE, EF;
+directional::TriMesh meshWhole, meshCut[NUM_N];
+directional::FaceField rawField[NUM_N], combedField[NUM_N];
 Eigen::VectorXi DPolyMesh[NUM_N];
 Eigen::MatrixXi FPolyMesh[NUM_N];
 Eigen::MatrixXd VPolyMesh[NUM_N];
-Eigen::VectorXi singIndices[NUM_N], singVertices[NUM_N];
 Eigen::MatrixXd NFunction[NUM_N], NCornerFunction[NUM_N];
 directional::DirectionalViewer viewer;
 
@@ -83,45 +75,43 @@ int main()
   "  2  Show isoline mesh" << std::endl <<
   "  3  change between different N" << std::endl;
   
-  igl::readOFF(TUTORIAL_SHARED_PATH "/vase.off", VMeshWhole, FMeshWhole);
-  directional::read_raw_field(TUTORIAL_SHARED_PATH "/vase-4.rawfield", N[0], rawField[0]);
-  directional::read_raw_field(TUTORIAL_SHARED_PATH "/vase-7.rawfield", N[1], rawField[1]);
-  directional::read_raw_field(TUTORIAL_SHARED_PATH "/vase-11.rawfield", N[2], rawField[2]);
-  igl::edge_topology(VMeshWhole, FMeshWhole, EV, FE, EF);
-  
+  directional::readOFF(TUTORIAL_SHARED_PATH "/vase.off",meshWhole);
+  directional::read_raw_field(TUTORIAL_SHARED_PATH "/vase-4.rawfield", meshWhole, N[0], rawField[0]);
+  directional::read_raw_field(TUTORIAL_SHARED_PATH "/vase-7.rawfield", meshWhole, N[1], rawField[1]);
+  directional::read_raw_field(TUTORIAL_SHARED_PATH "/vase-11.rawfield", meshWhole, N[2], rawField[2]);
+
   bool verbose=true;
   
   //combing and cutting
   for (int i=0;i<NUM_N;i++){
-    directional::principal_matching(VMeshWhole, FMeshWhole,EV, EF, FE, rawField[i], matching[i], effort[i],singVertices[i], singIndices[i]);
+    directional::principal_matching(rawField[i]);
 
     directional::IntegrationData intData(N[i]);
     std::cout<<"Setting up Integration #"<<i<<std::endl;
-    directional::setup_integration(VMeshWhole, FMeshWhole,  EV, EF, FE, rawField[i], matching[i], singVertices[i], intData, VMeshCut[i], FMeshCut[i], combedField[i], combedMatching[i]);
+    directional::setup_integration(meshWhole, rawField[i], intData, meshCut[i], combedField[i]);
     
     intData.verbose=false;
     intData.integralSeamless=true;
     intData.roundSeams=false;
   
     std::cout<<"Solving integration for N="<<N[i]<<std::endl;
-    directional::integrate(VMeshWhole, FMeshWhole, FE, combedField[i],  intData, VMeshCut[i], FMeshCut[i], NFunction[i],NCornerFunction[i]);
+    directional::integrate(meshWhole, combedField[i],  intData, meshCut[i], NFunction[i],NCornerFunction[i]);
     
     std::cout<<"Done!"<<std::endl;
     
     //setting up mesh data from itnegration data
     directional::MeshFunctionIsolinesData mfiData;
-    directional::setup_mesh_function_isolines(VMeshCut[i], FMeshCut[i], intData, mfiData);
+    directional::setup_mesh_function_isolines(meshCut[i], intData, mfiData);
     
     //meshing and saving
-    directional::mesh_function_isolines(VMeshWhole, FMeshWhole,EV, EF, FE, mfiData,  verbose, VPolyMesh[i], DPolyMesh[i], FPolyMesh[i]);
+    directional::mesh_function_isolines(meshWhole, mfiData,  verbose, VPolyMesh[i], DPolyMesh[i], FPolyMesh[i]);
     hedra::polygonal_write_OFF(TUTORIAL_SHARED_PATH "/vase-"+std::to_string(N[i])+"-generated.off", VPolyMesh[i], DPolyMesh[i], FPolyMesh[i]);
     
     
-    viewer.set_mesh(VMeshWhole, FMeshWhole,i);
-    viewer.set_field(combedField[i], directional::DirectionalViewer::indexed_glyph_colors(combedField[i]), i);
-    viewer.set_singularities(singVertices[i], singIndices[i]);
-    viewer.set_seams(combedMatching[i], i);
-    viewer.set_isolines(VMeshCut[i], FMeshCut[i],NFunction[i],i);
+    viewer.set_mesh(meshWhole,i);
+    viewer.set_field(combedField[i], directional::DirectionalViewer::indexed_glyph_colors(combedField[i].extField), i);
+    viewer.set_seams(combedField[i].matching, i);
+    viewer.set_isolines(meshCut[i] ,NFunction[i],i);
   }
   
   update_viewer();
