017413210981a9efbdcd40f4ab60a416eba6b468
Amir Vaxman <>
2022-06-30 13:05:49 +0200

    Still working on vertex-based fields.

--- include/directional/TriMesh.h
+++ include/directional/TriMesh.h
@@ -20,6 +20,8 @@
 #include <igl/boundary_loop.h>
 #include <igl/triangle_triangle_adjacency.h>
 #include <igl/gaussian_curvature.h>
+#include <igl/doublearea.h>
+#include <directional/dcel.h>
 
 namespace directional{
 
@@ -30,14 +32,17 @@ public:
   Eigen::MatrixXd V;
   Eigen::MatrixXi F;
   
-  //Topological quantities
-  Eigen::MatrixXi EF, FE, EV,TT, EFi;
+  //Topological quantities (TODO: proper VE and VF with boundaries)
+  Eigen::MatrixXi EF, FE, EV,TT, EFi, VE, VF;
   Eigen::MatrixXd FEs;
-  Eigen::VectorXi innerEdges, boundEdges;
+  Eigen::VectorXi innerEdges, boundEdges, vertexValence;
   
   //Geometric quantities
   Eigen::MatrixXd faceNormals;
-  Eigen::MatrixXd Bx,By;  //local basis vectors per face
+  Eigen::MatrixXd faceAreas;
+  Eigen::MatrixXd vertexNormals;
+  Eigen::MatrixXd FBx,FBy;  //local basis vectors per face
+  Eigen::MatrixXd VBx,VBy;  //local basis vectors per vertex
   Eigen::MatrixXd barycenters;
   Eigen::MatrixXd GaussianCurvature;
   int eulerChar;
@@ -98,12 +103,49 @@ public:
     innerEdges = Eigen::Map<Eigen::VectorXi, Eigen::Unaligned>(innerEdgesList.data(), innerEdgesList.size());
     boundEdges = Eigen::Map<Eigen::VectorXi, Eigen::Unaligned>(boundEdgesList.data(), boundEdgesList.size());
     igl::barycenter(V, F, barycenters);
-    igl::local_basis(V, F, Bx, By, faceNormals);
+    igl::local_basis(V, F, FBx, FBy, faceNormals);
     igl::triangle_triangle_adjacency(F, TT);
     igl::boundary_loop(F, boundaryLoops);
     igl::gaussian_curvature(V,F,GaussianCurvature);
+    igl::doublearea(V,F,faceAreas);
+    faceAreas.array()/=2.0;
     eulerChar = V.rows() - EV.rows() + F.rows();
     numGenerators = (2 - eulerChar)/2 - boundaryLoops.size();
+    
+    //generating VE and VF
+    Eigen::VectorXi VH,HV,HE,HF,nextH,prevH,twinH;
+    Eigen::MatrixXi EH,FH;
+    hedra::dcel(Eigen::VectorXi::Constant(F.rows()),F,EV,EF,EFi, innerEdges,VH,EH,FH,HV,HE,HF,nextH,prevH,twinH);
+    vertexValence=Eigen::VectorXi::Zero(V.rows());
+    for (int i=0;i<EV.rows();i++){
+      vertexValence(EV(i,0))++;
+      vertexValence(EV(i,1))++;
+    }
+    
+    //TODO: adapt to boundaries
+    VE.resize(V.rows(),vertexValence.maxCoeff());
+    VF.resize(V.rows(),vertexValence.maxCoeff());
+    for (int i=0;i<V.rows();i++){
+      int counter=0;
+      int hebegin = VH(i);
+      int heiterate = hebegin;
+      do{
+        VE(i,counter)=HE(heiterate);
+        VF(i,counter++)=HF(heiterate);
+        heiterate = twinH(prevH(heiterate));
+      }while(hebegin!=heiterate);
+    }
+    
+    //computing vertex normals by area-weighted aveage of face normals
+    vertexNormals=Eigen::MatrixXd::Zero(V.rows(),3);
+    for (int i=0;i<F.rows();i++)
+      for (int j=0;j<3;j++)
+        vertexNormals.row(F(i,j)).array()+=faceNormals.row(i)*faceAreas(i);
+    
+    vertexNormals.rowwise().normalize();
+    
+    //computing local basis that aligns with the first projected edge of each triangle
+    
   }
 
 };
--- include/directional/VertexField.h
+++ include/directional/VertexField.h
@@ -119,14 +119,25 @@ public:
     source = _source;
     face = _face;
     
+    extField.conservativeResize(intField.rows(),intField.cols()*3/2);
+    for (int i=0;i<intField.rows();i++)
+      for (int j=0;j<intField.cols();j+=2)
+        extField.block(i,3*j/2,1,3)=mesh->VBx.row(i)*intField(i,j)+mesh->VBy.row(i)*intField(i,j+1);
+    
    
-    if (face.rows()==0){
+
+    /*if (face.rows()==0){
       intField.resize(extField.rows(),2*N);
       for (int i=0;i<V.rows();i++){
         for (int j=0;j<N;j++){
           RowVector3d extVector = extField.block(i,3*j,1,3);
+          
+          //projecting on the flat tangent space of the vertex normal
+          
+          
+          
           //projecting on all face tangents and choosing the closest
-          int closestVector=-1;
+          /*int closestVector=-1;
           int maxCosDist = -3276700.0;
           RowVector3d finalIntVector;
           //TODO: handle boundaries
@@ -146,7 +157,7 @@ public:
           double angleDiff = std::acos(closestEdgeVector.dot(finalIntVector)./closestEdgeVector.norm()/finalIntVector.norm());
           std::complex<double> complexIntVector=finalIntVector.norm()*complex(exp(0,tangentStartAngles(closestVector+angleDiff)));
           intField.block(i,2*j,1,2)=complexIntVector;
-        }
+        }*/
       }
         
         
@@ -159,82 +170,71 @@ public:
     }
   }
   
-  void IGL_INLINE set_intrinsic_field(const Eigen::MatrixXd& _intField){
-    assert (!(fieldType==POWER_FIELD) || (_intField.cols()==2));
-    assert ((_intField.cols()==2*N) || !(fieldType==POLYVECTOR_FIELD || fieldType==RAW_FIELD));
-    intField = _intField;
-    
-    //computing extrinsic field
-    //TODO: do the extFIeld properly and the angle scaling also.
-    extField.conservativeResize(intField.rows(),intField.cols()*3/2);
-    for (int i=0;i<intField.rows();i++)
-      for (int j=0;j<intField.cols();j+=2){
-        vecAngle = log(std::complex<double>(intField(i,j),intField(i,j+1))).imag();
-        while (vecAngle<0.0) vecAngle+=2*igl::PI;
-        while (vecAngle>2*igl::PI) vecAngle-=2*igl::PI;
-        
-        //finding face
-        for (int k=0;k<mesh->valences(i)-1;k++)
-          if ((vecAngle>=tangentStartAngles(i,k))&&(vecAngle<=tangentStartAngles(i,k+1))
-              extField.block(i,3*j/2,1,3)=mesh->Bx.row(i)*intField(i,j)+mesh->By.row(i)*intField(i,j+1);
-            
-      }
-        
-  }
+void IGL_INLINE set_intrinsic_field(const Eigen::MatrixXd& _intField){
+  assert (!(fieldType==POWER_FIELD) || (_intField.cols()==2));
+  assert ((_intField.cols()==2*N) || !(fieldType==POLYVECTOR_FIELD || fieldType==RAW_FIELD));
+  intField = _intField;
   
-  void IGL_INLINE set_intrinsic_field(const Eigen::MatrixXcd& _intField){
-    assert (!(fieldType==POWER_FIELD) || (_intField.cols()==1));
-    assert ((_intField.cols()==N) || !(fieldType==POLYVECTOR_FIELD || fieldType==RAW_FIELD));
-    
-    intField.resize(_intField.rows(),_intField.cols()*2);
-    for (int i=0;i<_intField.cols();i++){
-      intField.col(2*i)=_intField.col(i).real();
-      intField.col(2*i+1)=_intField.col(i).imag();
-    }
-    //computing extrinsic field
-    extField.conservativeResize(_intField.rows(),_intField.cols()*3);
-    for (int i=0;i<_intField.rows();i++)
-      for (int j=0;j<_intField.cols();j++)
-        extField.block(i,3*j,1,3)=mesh->Bx.row(i)*_intField(i,j).real()+mesh->By.row(i)*_intField(i,j).imag();
-  }
+  //computing extrinsic field
+  extField.conservativeResize(intField.rows(),intField.cols()*3/2);
+  for (int i=0;i<intField.rows();i++)
+    for (int j=0;j<intField.cols();j+=2)
+      extField.block(i,3*j/2,1,3)=mesh->VBx.row(i)*intField(i,j)+mesh->VBy.row(i)*intField(i,j+1);
+}
+
+void IGL_INLINE set_intrinsic_field(const Eigen::MatrixXcd& _intField){
+  assert (!(fieldType==POWER_FIELD) || (_intField.cols()==1));
+  assert ((_intField.cols()==N) || !(fieldType==POLYVECTOR_FIELD || fieldType==RAW_FIELD));
   
-  Eigen::MatrixXd  virtual IGL_INLINE project_to_intrinsic(const Eigen::VectorXi& tangentSpaces, const Eigen::MatrixXd& extDirectionals) const{
-    assert(tangentSpaces.rows()==extDirectionals.rows());
-    Eigen::MatrixXd intDirectionals(tangentSpaces.rows(),2);
-    
-    //std::cout<<"tangentSpaces: "<<tangentSpaces<<std::endl;
-    for (int j=0;j<tangentSpaces.rows();j++){
-      /*std::cout<<"j: "<<j<<std::endl;
-      std::cout<<"(extDirectionals.row(j).array()*mesh->Bx.row(tangentSpaces(j)).array()).sum(),(extDirectionals.row(j).array()*mesh->By.row(tangentSpaces(j)).array()).sum():"<<(extDirectionals.row(j).array()*mesh->Bx.row(tangentSpaces(j)).array()).sum()<<","<<(extDirectionals.row(j).array()*mesh->By.row(tangentSpaces(j)).array()).sum()<<std::endl;*/
-      intDirectionals.row(j)<<(extDirectionals.row(j).array()*mesh->Bx.row(tangentSpaces(j)).array()).sum(),(extDirectionals.row(j).array()*mesh->By.row(tangentSpaces(j)).array()).sum();
-    }
-    return intDirectionals;
+  intField.resize(_intField.rows(),_intField.cols()*2);
+  for (int i=0;i<_intField.cols();i++){
+    intField.col(2*i)=_intField.col(i).real();
+    intField.col(2*i+1)=_intField.col(i).imag();
   }
+  //computing extrinsic field
+  extField.conservativeResize(_intField.rows(),_intField.cols()*3);
+  for (int i=0;i<_intField.rows();i++)
+    for (int j=0;j<_intField.cols();j++)
+      extField.block(i,3*j,1,3)=mesh->VBx.row(i)*_intField(i,j).real()+mesh->VBy.row(i)*_intField(i,j).imag();
+}
+
+Eigen::MatrixXd  virtual IGL_INLINE project_to_intrinsic(const Eigen::VectorXi& tangentSpaces, const Eigen::MatrixXd& extDirectionals) const{
+  assert(tangentSpaces.rows()==extDirectionals.rows());
+  Eigen::MatrixXd intDirectionals(tangentSpaces.rows(),2);
+  
+  //std::cout<<"tangentSpaces: "<<tangentSpaces<<std::endl;
+  for (int j=0;j<tangentSpaces.rows();j++){
+    /*std::cout<<"j: "<<j<<std::endl;
+    std::cout<<"(extDirectionals.row(j).array()*mesh->Bx.row(tangentSpaces(j)).array()).sum(),(extDirectionals.row(j).array()*mesh->By.row(tangentSpaces(j)).array()).sum():"<<(extDirectionals.row(j).array()*mesh->Bx.row(tangentSpaces(j)).array()).sum()<<","<<(extDirectionals.row(j).array()*mesh->By.row(tangentSpaces(j)).array()).sum()<<std::endl;*/
+    intDirectionals.row(j)<<(extDirectionals.row(j).array()*mesh->VBx.row(tangentSpaces(j)).array()).sum(),(extDirectionals.row(j).array()*mesh->VBy.row(tangentSpaces(j)).array()).sum();
+  }
+  return intDirectionals;
+}
     
-  void IGL_INLINE set_singularities(const Eigen::VectorXi& _singVertices,
+  void IGL_INLINE set_singularities(const Eigen::VectorXi& _singFaces,
                                     const Eigen::VectorXi& _singIndices){
     
-    Eigen::VectorXi vertexIndices=Eigen::VectorXi::Zero(mesh->V.rows());
-    int singCounter=_singVertices.size();
-    for (int i=0;i<_singVertices.size();i++)
-      vertexIndices(_singVertices(i))=_singIndices(i);
+    Eigen::VectorXi faceIndices=Eigen::VectorXi::Zero(mesh->F.rows());
+    int singCounter=_singFaces.size();
+    for (int i=0;i<_singFaces.size();i++)
+      faceIndices(_singFaces(i))=_singIndices(i);
     
     //removing boundary indices
-    std::vector<std::vector<int> > L;
+   /* std::vector<std::vector<int> > L;
     igl::boundary_loop(mesh->F, L);
     for (int j=0;j<L.size();j++)
       for (int k=0;k<L[j].size();k++){
         if (vertexIndices(L[j][k])) singCounter--;
         vertexIndices(L[j][k])=0;
-      }
+      }*/
     
     singCycles.resize(singCounter);
     singIndices.resize(singCounter);
     singCounter=0;
-    for (int i=0;i<mesh->V.rows();i++){
-      if (vertexIndices(i)){
+    for (int i=0;i<mesh->F.rows();i++){
+      if (faceIndices(i)){
         singCycles(singCounter)=i;
-        singIndices(singCounter++)=vertexIndices(i);
+        singIndices(singCounter++)=faceIndices(i);
       }
     }
   }
