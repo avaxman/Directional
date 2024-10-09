01835890f69e9556372d9f94ae69118387eed24b
Amir Vaxman <avaxman@gmail.com>
2024-10-09 23:43:28 +0100

    almost done with 602.

---
+++ include/directional/dec.h
@@ -0,0 +1,98 @@
+// This file is part of Directional, a library for directional field processing.
+// Copyright (C) 2024 Amir Vaxman <avaxman@gmail.com>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License
+// v. 2.0. If a copy of the MPL was not distributed with this file, You can
+// obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef DIRECTIONAL_DEC_H
+#define DIRECTIONAL_DEC_H
+
+#include <eigen/sparse>
+#include <directional/TriMesh.h>
+
+namespace directional {
+
+    template<typename NumberType>
+    Eigen::SparseMatrix<NumberType> d0_matrix(const TriMesh *mesh,
+                                              const bool isDirichlet=false) {
+        Eigen::SparseMatrix<NumberType> d0(mesh->EV.rows(), mesh->V.rows());
+        std::vector<Eigen::Triplet<NumberType>> d0Tris;
+        for (int i=0;i<mesh->EV.rows();i++) {
+            d0Tris.push_back(Eigen::Triplet<NumberType>(i, mesh->EV(i, 0), -1.0));
+            d0Tris.push_back(Eigen::Triplet<NumberType>(i, mesh->EV(i, 1), 1.0));
+        }
+        d0.setFromTriplets(d0Tris.begin(), d0Tris.end());
+        return d0;
+    }
+
+    template<typename NumberType>
+    Eigen::SparseMatrix<NumberType> d1_matrix(const TriMesh *mesh,
+                                              const bool isDirichlet=false) {
+        Eigen::SparseMatrix<NumberType> d1(mesh->F.rows(), mesh->EF.rows());
+        std::vector<Eigen::Triplet<NumberType>> d1Tris;
+        for (int i=0;i<mesh->FE.rows();i++)
+            for (int j=0;j<3;j++)
+                d1Tris.push_back(Eigen::Triplet<NumberType>(i, mesh->FE(i, j), mesh->FEs(i,j)));
+
+        d1.setFromTriplets(d1Tris.begin(), d1Tris.end());
+        return d1;
+    }
+
+    //The primal/dual diagonal hodge star (with a choice of center so as to make it positive)
+    template<typename NumberType>
+    void hodge_star_1_matrix(const TriMesh *mesh,
+                             Eigen::SparseMatrix<NumberType> hodgeStar,
+                             Eigen::SparseMatrix<NumberType> invHodgeStar,
+                             const bool circumcenter = true,
+                             const bool isDirichlet = false) {
+
+        /*Eigen::VectorXd primalEdgeLengths(mesh->EV.rows());
+        Eigen::VectorXd dualEdgeLengths(mesh->EV.rows());*/
+        Eigen::VectorXd M1Weights(mesh->EV.rows());
+
+        for (int i=0;i<mesh->EV.rows();i++){
+            if (!circumcenter) {
+                double primalEdgeLengths = (mesh->V.row(mesh->EV(i, 0)) - mesh->V.row(mesh->EV(i, 1))).norm();
+                double dualEdgeLength=0.0;
+                Eigen::RowVector3d midEdge = (mesh->V.row(mesh->EV(i, 0)) + mesh->V.row(mesh->EV(i, 1))) / 2.0;
+                if (mesh->EF(i,0)!=0) {
+                    Eigen::RowVector3d leftBarycenter = mesh->barycenters.row(mesh->EF(i, 0));
+                    dualEdgeLength += (leftBarycenter - midEdge).norm();
+                }
+                if (mesh->EF(i,1)!=0) {
+                    Eigen::RowVector3d rightBarycenter = mesh->barycenters.row(mesh->EF(i, 1));
+                    dualEdgeLength += (rightBarycenter - midEdge).norm();
+                }
+                M1Weights(i)=dualEdgeLength/primalEdgeLengths;
+            } else {//cot weights
+                M1Weights(i)=0.0;
+                for (int j=0;j<2;j++){
+                    if (mesh->EF(i,j)==-1)
+                        continue;
+                    int nextHe = mesh->nextH(mesh->EH(i,j));
+                    int prevHe = mesh->prevH(mesh->EH(i,j));
+
+                    Eigen::RowVector3d e1 = -(mesh->V.row(mesh->HV(mesh->nextH(nextHe)))-mesh->V.row(mesh->HV(nextHe)));
+                    Eigen::RowVector3d e2 = mesh->V.row(mesh->HV(mesh->nextH(prevHe)))-mesh->V.row(mesh->HV(prevHe));
+                    double sinAngle = e1.cross(e2).norm();
+                    double cosAngle = e1.dot(e2);
+                    if (std::abs(sinAngle)<10e-7)
+                        continue;  //using 0 weight
+
+                    M1Weights(i)+=cosAngle/sinAngle;
+                }
+            }
+        }
+
+        hodgeStar = directional::sparse_diagonal(M1Weights);
+        Eigen::VectorXd invWeights = M1Weights.unaryExpr([](double v) {
+            return (std::abs(v)>10e-7 ? 1.0/v : 0.0);
+        });
+        invHodgeStar = directional::sparse_diagonal(invWeights);
+    }
+
+
+}
+
+#endif //DIRECTIONAL_DEC_H
---
+++ tutorial/602_DiscreteExteriorCalculus/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <Eigen/Core>
+#include <directional/readOFF.h>
+#include <directional/TriMesh.h>
+#include <directional/CartesianField.h>
+#include <directional/directional_viewer.h>
+#include <directional/dec.h>
+
+directional::TriMesh mesh;
+Eigen::VectorXd z1Exact, z1Coexact;
+directional::DirectionalViewer viewer;
+
+
+int main()
+{
+    directional::readOFF(TUTORIAL_DATA_PATH "/Pegasus.off",mesh);
+
+    Eigen::VectorXd z0(mesh.dcel.vertices.size()), z2dual(mesh.dcel.edges.size());
+    for (int i=0;i<mesh.dcel.vertices.size();i++)
+        z0[i] = sin(mesh.V(i,0)/20.0)*cos(mesh.V(i,1)/20.0);
+    for (int i=0;i<mesh.dcel.edges.size();i++){
+        Eigen::RowVector3d midEdgePoint = 0.5*(mesh.V.row(mesh.EV(i,0))+mesh.V.row(mesh.EV(i,1)));
+        z2dual[i] = midEdgePoint(0); //sin(midEdgePoint(1)/20.0)*cos(midEdgePoint(2)/20.0);
+    }
+
+    Eigen::SparseMatrix<double> d0 = directional::d0_matrix<double>(&mesh);
+    Eigen::SparseMatrix<double> d1 = directional::d1_matrix<double>(&mesh);
+    Eigen::SparseMatrix<double> hodgeStar, invHodgeStar;
+    directional::hodge_star_1_matrix(&mesh, hodgeStar, invHodgeStar);
+
+    z1Exact = d0*z0;
+    z1Coexact =invHodgeStar * d1.transpose() * z2dual;
+
+    //demonstrating the exact sequences
+    Eigen::SparseMatrix<double> d1d0 = d1*d0;
+    double maxAbsValue = 0.0;
+    for (int k = 0; k < d1d0.outerSize(); ++k)
+        for (Eigen::SparseMatrix<double>::InnerIterator it(d1d0, k); it; ++it)
+            maxAbsValue = std::max(maxAbsValue, std::abs(it.value()));
+
+
+    std::cout<<"exact sequence identity (should be numerically zero): "<<maxAbsValue<<std::endl;
+
+    //triangle mesh setup
+    viewer.init();
+    viewer.set_mesh(mesh);
+    viewer.set_vertex_data(z0, z0.minCoeff(), z0.maxCoeff(),"Primal 0-form", 0);
+    viewer.set_1form(z1Exact,"Gradient field", 0, 0, 20.0);
+    viewer.set_face_data(z2dual, z2dual.minCoeff(), z2dual.maxCoeff(),"dual 0-form", 0);
+    viewer.set_1form(z1Coexact,"Rot. Cogradient field", 0, 1, 10.0);
+    viewer.launch();
+}
