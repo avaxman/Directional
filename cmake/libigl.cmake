f03b7ea8fe3e7353f189c2841a7524c395d6eabf
Amir Vaxman <avaxman@gmail.com>
2025-01-07 11:40:57 +0000

    changing libigl to local fork.

--- cmake/libigl.cmake
+++ cmake/libigl.cmake
@@ -5,7 +5,7 @@ endif()
 include(FetchContent)
 FetchContent_Declare(
     libigl
-    GIT_REPOSITORY https://github.com/libigl/libigl.git
+    GIT_REPOSITORY https://github.com/avaxman/libigl.git
     GIT_TAG v2.5.0
 )
 FetchContent_MakeAvailable(libigl)
